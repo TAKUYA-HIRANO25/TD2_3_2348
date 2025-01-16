ae723b6b0b45aea960ecb36ef82f25a690a627b7
LC1C_22_トウ_カグン <146914486+DOUJIAQUN@users.noreply.github.com>
2025-01-16 11:18:55 +0900

    複数の敵

--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -3,6 +3,7 @@
 #include <KamataEngine.h>
 #include "Time.h"
 #include "Fade.h"
+#include "Enemy.h"
 
 /// <summary>
 /// ゲームシーン
@@ -39,12 +40,28 @@ public: // メンバ関数
 
 	void ChangePhase();
 
+	void EnemyPop(KamataEngine::Vector3 positon);
+
+	/// 敵発生データの読み込み
+	void LoadEnemyPopData();
+
+	// 敵発生コマンドの更新
+	void UpdateEnemyPopCommands();
+
+	const KamataEngine::Vector3 screenCenter = { 0.0f, 0.0f, 0.0f }; // 屏幕中心坐标
+
 private: // メンバ変数
 
 	KamataEngine::DirectXCommon* dxCommon_ = nullptr;
 	KamataEngine::Input* input_ = nullptr;
 	KamataEngine::Audio* audio_ = nullptr;
-	
+
+
+	uint32_t EnemytextureHandle_ = 0;
+	// 3Dモデル
+	KamataEngine::Model* model_ = nullptr;
+	// ビュープロジェクション
+	KamataEngine::Camera camera_;
 
 	//時間
 	Time* time_ = nullptr;
@@ -66,7 +83,27 @@ private: // メンバ変数
 	};
 	// ゲームの現在のフェーズ
 	Phase phase_;
+
+	// 敌人
+	Enemy* enemy_ = nullptr;
+	std::list<Enemy*> enemys_;
+	// 敵の座標
+	KamataEngine::Vector3 Position = { 2.0f, 0.0f, 60.0f };
+	// 敵の速度
+	KamataEngine::Vector3 Velocity_ = { 0, 0, -0.1f };
+	// 敵の離脱時速度
+	KamataEngine::Vector3 LeaveVelo_ = { -1.0f, 1.0f, 0.0f };
+	// 敵の半径
+	float Enemyradius_ = 1.0f;
+
+	// 敵発生コマンド
+	std::stringstream enemyPopCommands;
+	// 待機中フラグ
+	bool waitFlag = false;
+	// 待機タイマー
+	int32_t waitTimer = 0;
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
+	
 };
